b869446cf4a3add50e05c7b62481f7e1629acbe3
Dev-Drift <devdrifttech@gmail.com>
2025-01-18 13:50:33 +0500

    Update App.js test

--- devdriftwebreact/src/App.js
+++ devdriftwebreact/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Dev Driftsss
+          Dev Drift
         </p>
         <a
           className="App-link"
