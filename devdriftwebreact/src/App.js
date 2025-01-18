4e3a7ebf85b76db8766efd2b315cf7f5b6d626a3
Dev-Drift <devdrifttech@gmail.com>
2025-01-18 13:49:54 +0500

    sss test

--- devdriftwebreact/src/App.js
+++ devdriftwebreact/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Dev Drift
+          Dev Driftsss
         </p>
         <a
           className="App-link"
